1c4a69eedf39ac0670fccb1cb7ef83fe7df72030
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-12-07 02:15:34 +0000

    Create Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,24 @@
 // Navbar Component
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="mt-7 p-5 justify-between bg-gray-800 w-screen inline-block md:inline-flex lg:inline-flex xl:inline-flex ">
+      <Link to="/">
+        <h1 className="pl-5 font-bold text-gray-900 dark:text-white text-3xl sm:text-2xl">
+          MOVIE SEARCH APP
+        </h1>
+      </Link>
+
+      {/* Navigation Links */}
+      <nav className="inline-flex">
+        <Link to="/" className="p-5">
+          Home
+        </Link>
+        <Link to="/favorites" className="p-5">
+          My Favorites
+        </Link>
+      </nav>
     </div>
   );
 }
