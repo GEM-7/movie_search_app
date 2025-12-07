578805ae4a4ce5867e490f4a0d5cae2cb07db808
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-12-07 02:19:35 +0000

    Create 404-Page Not Found

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,14 @@
 // PageNotFound Component
+import { Link } from "react-router-dom";
+
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div className="text-center mt-35">
+      <h1 className="text-2xl font-bold ">404 - Page Not Found</h1>
+      <p className="mt-5">The page you are looking for does not exist.</p>
+      <Link to="/" className="mt-5 text-blue-500">
+        Go back to Home
+      </Link>
     </div>
   );
 }
