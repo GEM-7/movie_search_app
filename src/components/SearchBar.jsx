24cf5573adbfb1a32a23bd3993819dd910f904a9
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-12-31 00:02:10 +0000

    Update search component to fecth data request from API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,24 @@
 // SearchBar Component
+import { useMovieStore } from "../stores/movieSearchStore";
+import { useState } from "react";
+
 export default function SearchBar() {
+  // Use Movie Store
+  const { setSearchQuery: setStoreSearchQuery, searchMovies } = useMovieStore();
+  // Handle Search
+  const handleSearch = (query) => {
+    setStoreSearchQuery(query);
+    if (query.trim()) {
+      searchMovies(query);
+    }
+  };
+  // Handle Submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch(searchQuery);
+  };
+  // Search Query
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <div className="flex justify-center mt-14 w-full">
       {" "}
@@ -8,21 +27,29 @@ export default function SearchBar() {
         <input
           type="text"
           placeholder="Search..."
-          className="w-90 pl-4 pr-10 py-2 border border-gray-300 focus:outline-none focus:ring-2 rounded-4xl focus:ring-beige-500"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSubmit(e)}
+          className="w-90 pl-4 pr-10 py-2 border border-gray-300 focus:outline-none focus:ring-2 rounded-4xl focus:ring-orange-500"
         />{" "}
-        <svg
-          className="absolute right-3 h-5 w-5 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+        <button
+          onClick={handleSubmit}
+          className="absolute right-3 h-5 w-5 text-gray-400 hover:text-orange-500"
         >
-          {" "}
-          <path
-            fillRule="evenodd"
-            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.448 4.272l3.022 3.022a.75.75 0 11-1.06 1.06l-3.022-3.022A7 7 0 012 9z"
-            clipRule="evenodd"
-          />{" "}
-        </svg>{" "}
+          <svg
+            className="w-full h-full"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            {" "}
+            <path
+              fillRule="evenodd"
+              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.448 4.272l3.022 3.022a.75.75 0 11-1.06 1.06l-3.022-3.022A7 7 0 012 9z"
+              clipRule="evenodd"
+            />{" "}
+          </svg>
+        </button>{" "}
       </div>{" "}
     </div>
   );
