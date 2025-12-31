ad95aa9b65086a0154d62d4943a5da6a53eb8b41
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-12-31 00:11:12 +0000

    Render search data using movie card

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,26 @@
 // MovieList Component
 import SearchBar from "./SearchBar";
+import MovieCard from "./MovieCard";
+import { useMovieStore } from "../stores/movieSearchStore";
 
 export default function MovieList() {
+  const { movies, isLoading, error } = useMovieStore();
   return (
     <div>
+      {/* Search Bar */}
       <SearchBar className="mt-14" />
+      {/* Movie List */}
+      <div className="mt-10">
+        <h2 className="text-2xl font-bold ml-8 mb-4">Movie List</h2>
+
+        {isLoading && <p className="text-center font-extrabold text-gray-500">Loading...</p>}
+        {error && <p className="text-center font-extrabold text-red-500">Error: {error}</p>}
+
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-4 center">
+          {Array.isArray(movies) &&
+            movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+      </div>
     </div>
   );
 }
